85998208ea5676c6586a24024a34e37bd1154fb8
bradyvolk <39806319+bradyvolk@users.noreply.github.com>
2022-02-24 20:23:52 -0500

    Fix data processor for emojis

--- dialogue.py
+++ dialogue.py
@@ -14,19 +14,22 @@ def transform_dialogue():
     ]
     
     """  
-    list_of_dialog = json.loads(open('dialogue.json').read())
+    with open('dialogue.json', encoding='utf-8') as fh:
+        list_of_dialog = json.load(fh)
+          
+    # list_of_dialog = json.loads(open('dialogue.json').read())
     feedback = []
     
     for dialogue in list_of_dialog:
         
-        print("Dialogue:", dialogue)
+        # print("Dialogue:", dialogue)
         feedback_object = { "conversation": [], "rating": None}
-        list_of_responses = dialogue['dialogue']
+        list_of_responses = dialogue['dialog']
 
         # Find first human response
         first_h_response_index = None
         i = 0 
-        while (not first_h_response_index):
+        while not first_h_response_index and i < len(list_of_responses):
             if list_of_responses[i]['sender_class'] == 'Human':
                 first_h_response_index = i
             i += 1
@@ -34,23 +37,33 @@ def transform_dialogue():
         list_of_responses = list_of_responses[first_h_response_index:]      
         
         expecting_human_response = True
+        logged = False
         # Find pairs of human - bot responses
-        for i in range(len(list_of_responses)):
-            (human_responses, bot_responses) = "", ""
-            if expecting_human_response and list_of_responses[i]['sender_class'] == 'Human':
-                human_responses += list_of_responses[i]['text']
-            elif expecting_human_response and list_of_responses[i]['sender_class'] == 'Bot':
-                bot_responses += list_of_responses[i]['text']
+        (human_responses, bot_responses) = "", ""
+        for response in list_of_responses:
+            logged = False
+            if expecting_human_response and response['sender_class'] == 'Human':
+                human_responses += " " +  response['text'].strip()
+            elif expecting_human_response and response['sender_class'] == 'Bot':
+                bot_responses += " " + response['text'].strip()
                 expecting_human_response = False
-            elif not expecting_human_response and list_of_responses[i]['sender_class'] == 'Bot':
-                bot_responses += list_of_responses[i]['text']
-            else:
-                feedback_object['conversation'].append((human_responses, bot_responses))
-                (human_responses, bot_responses) = "", ""
+            elif not expecting_human_response and response['sender_class'] == 'Bot':
+                bot_responses += " " + response['text'].strip()
+            elif not expecting_human_response and response['sender_class'] == 'Human':
+                feedback_object['conversation'].append((human_responses.strip(), bot_responses.strip()))
+                (human_responses, bot_responses) = response['text'].strip(), ""
+                logged = True
+        
+        if not logged:    
+            feedback_object['conversation'].append((human_responses, bot_responses))
         
         feedback_object["rating"] = dialogue["eval_score"]
         feedback.append(feedback_object)
-        print(feedback_object, feedback)
+        
+    feedback_json = json.dumps(feedback)
+    
+    with open('processed_training_data.json', 'w') as outfile:
+        outfile.write(feedback_json)
         
 if __name__ == "__main__":
     transform_dialogue()    
